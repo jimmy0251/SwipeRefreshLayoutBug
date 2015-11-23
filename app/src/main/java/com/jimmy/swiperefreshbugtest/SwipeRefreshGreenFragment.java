29be067c14a5d0a54fd356f73fff6f1a91f85f34
jignesh sanghani <jignesh.sanghani@avinashi.com>
2015-11-23 14:47:11 +0530

    Initial commit

---
+++ app/src/main/java/com/jimmy/swiperefreshbugtest/SwipeRefreshGreenFragment.java
@@ -0,0 +1,32 @@
+package com.jimmy.swiperefreshbugtest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * Created by jigneshsanghani on 23/11/15.
+ */
+public class SwipeRefreshGreenFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_swipe_refresh_green, container, false);
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefresh);
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(true);
+            }
+        });
+        
+        Toast.makeText(getActivity(), "Inflated green fragment", Toast.LENGTH_LONG).show();
+        return view;
+    }
+}
